236370d393bed91a9aeac9f472cc978b48e587c2
madpluto <478915022@qq.com>
2017-07-21 19:01:19 +0800

    自动刷新

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,6 +73,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_jquery__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__foo__ = __webpack_require__(5);
+
 
 
 
@@ -80,12 +82,14 @@ function component () {
   var element = __WEBPACK_IMPORTED_MODULE_1_jquery___default()('<div></div>');
 
   /* lodash is required for the next line to work */
-  element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello','webpack'], ' '))
+  element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hi','webpack'], ' '))
 
   return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */])
+console.log(Object(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */])())
 
 
 /***/ }),
@@ -27494,6 +27498,15 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony default export */ __webpack_exports__["a"] = (function(){
+   return '方方的教程'
+});
 
 /***/ })
 /******/ ]);
